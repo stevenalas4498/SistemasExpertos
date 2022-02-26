3b0c950aac09ba9fdac447af35edf6d9842d9da6
stevenalas4498 <97303073+stevenalas4498@users.noreply.github.com>
2022-02-26 15:27:26 -0600

    Add files via upload

---
+++ factorial con lenguaje favorito/factorial.py
@@ -0,0 +1,9 @@
+def factorial(n): 
+    if(n == 1):
+        return 1
+    elif(n == 0):
+        return 1
+    else:
+        return n * factorial(n - 1)
+num=int(input("Ingrese el numero deseado: "))
+print(num,"! es = ", factorial(num))
